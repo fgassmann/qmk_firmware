ee08e593c0b6f24a587d3dd7d7744c96d81d9bc5
flo <floriangassmannfau@gmail.com>
2021-10-08 15:30:35 +0200

    readme and custom keycodes.h

---
+++ keyboards/gmmk/pro/iso/keymaps/fgassmann/custom_keycodes.h
@@ -0,0 +1,107 @@
+/* Copyright 2021 Glorious, LLC <salman@pcgamingrace.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//Unicode for entering german characters on an englisch layout
+enum unicode_names {
+    _oe,_OE, // ö Ö
+    _ae,_AE, // ä Ä
+    _ue,_UE, // ü Ü
+    _sz,_EU,  // ß €
+    //I left these in just for fun
+    IRONY,
+    SNEK
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+    [_oe] = 0xF6, [_OE] = 0xD6,  // ö Ö
+    [_ae] = 0xE4, [_AE] = 0xC4,  // ä Ä
+    [_ue] = 0xFC, [_UE] = 0xDC,  // ü Ü
+    [_sz] = 0xDF, [_EU] = 0x20AC, // ß €
+    [IRONY] = 0x2E2E,  // ⸮
+    [SNEK]  = 0x1F40D, // 🐍
+};
+
+// NOTE: making the characters Unicode symbols instead of macros would probably have been more elegant, however not all Latex editors support this
+enum custom_keycodes {
+  //LATEX GREEK
+    _TEX_A_= SAFE_RANGE,_TEX_a_, //Alpha
+    _TEX_B_, _TEX_b_,//Beta
+    _TEX_C_, _TEX_c_,//Psi
+    _TEX_D_, _TEX_d_,//Delta
+    _TEX_E_, _TEX_e_, //Epsilon, vareppsilon
+    _TEX_F_, _TEX_f_, //Phi
+    _TEX_G_, _TEX_g_, //Gamma
+    _TEX_H_, _TEX_h_, //Eta
+    _TEX_I_, _TEX_i_, //Iota
+    _TEX_J_, _TEX_j_, //XI
+    _TEX_K_, _TEX_k_, //Kappa
+    _TEX_L_, _TEX_l_, //Lambda
+    _TEX_M_, _TEX_m_, //Mu
+    _TEX_N_, _TEX_n_, //Nu
+    _TEX_O_, _TEX_o_, //Omicron
+    _TEX_P_, _TEX_p_, //Pi
+    _TEX_Q_, _TEX_q_, // ??
+    _TEX_R_, _TEX_r_, //Rho
+    _TEX_S_, _TEX_s_, //Sigma
+    _TEX_T_, _TEX_t_, //Tau
+    _TEX_U_, _TEX_u_, //Theta
+    _TEX_V_, _TEX_v_, //Omega
+    _TEX_W_, _TEX_w_, // ??
+    _TEX_X_, _TEX_x_, //Chi
+    _TEX_Y_, _TEX_y_, //Upsilon
+    _TEX_Z_, _TEX_z_, //Zeta
+  //LATEX FUNCTIONS
+    //Environments
+    _BMAT,
+    _MAT,
+    _PMAT,
+    _VMAT,
+    _LCAS,
+    _RCAS,
+    //Spacing
+    _TEX_SPACE,
+    _TEX_QUAD,
+    //Arrows
+    _UP_AR,
+    _LE_AR,
+    _DN_AR,
+    _RG_AR
+};
+
+static char *tex_greek_chars[] ={
+  "Alpha","alpha","Beta","beta","Psi","psi","Delta","delta","Epsilon","varepsilon","Phi","phi","Gamma","gamma","Eta","eta",
+  "Iota","iota","Xi","xi","Kappa","kappa","Lambda","lambda","Mu","mu","Nu","nu","Omicron","omicron","Pi","pi","x","x","Rho",
+  "rho","Sigma","sigma","Tau","tau","Theta","theta","Omega","omega","x","x","Chi","chi","Upsilon","upsilon","Zeta","zeta"
+};
+static char *tex_functions[] ={
+  //Spacing
+  "space",
+  "quad",
+  //Single Arrows
+  "uarr", 
+  "larr",
+  "darr",
+  "rarr"
+};
+static char *tex_environments[] ={
+  //Environments
+  "bmatrix", // Matrix []
+  "matrix",  // Matrix without brackets
+  "pmatrix", // Matrix ()
+  "vmatrix", // Matrix ||
+  "cases",   // Cases  {
+  "rcases"   // Cases  }
+};
--- keyboards/gmmk/pro/iso/keymaps/fgassmann/keymap.c
+++ keyboards/gmmk/pro/iso/keymaps/fgassmann/keymap.c
@@ -16,7 +16,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 #include "rgb_matrix_map.h"
-#include "keymap_german.h"
+//#include "keymap_german.h"
+#include "custom_keycodes.h"
 
 //Layers
 enum custom_layers {
@@ -31,95 +32,7 @@ enum custom_layers {
   _KYBS // Keyboard settings
 
 };
-//Unicode
-enum unicode_names {
-    _oe,_OE, // ö Ö
-    _ae,_AE, // ä Ä
-    _ue,_UE, // ü Ü
-    _sz,     // ß
-    IRONY,
-    SNEK
-};
-const uint32_t PROGMEM unicode_map[] = {
-    [_oe] = 0xF6, [_OE] = 0xD6, // ö Ö
-    [_ae] = 0xE4, [_AE] = 0xC4,
-    [_ue] = 0xFC, [_UE] = 0xDC,
-    [_sz] = 0xDF,
-    [IRONY] = 0x2E2E,  // ⸮
-    [SNEK]  = 0x1F40D, // 🐍
-};
 
-
-enum custom_keycodes {
-  //LATEX GREEK
-    _TEX_A_= SAFE_RANGE,_TEX_a_, //Alpha 1.
-    _TEX_B_, _TEX_b_,//Beta 2.
-    _TEX_C_, _TEX_c_,//Psi 23.
-    _TEX_D_, _TEX_d_,//Delta 4.
-    _TEX_E_, _TEX_e_, //Epsilon, vareppsilon 5.
-    _TEX_F_, _TEX_f_, //Phi 21.
-    _TEX_G_, _TEX_g_, //Gamma 3.
-    _TEX_H_, _TEX_h_, //Eta 7.
-    _TEX_I_, _TEX_i_, //Iota 9.
-    _TEX_J_, _TEX_j_, //XI 14.
-    _TEX_K_, _TEX_k_, //Kappa 10.
-    _TEX_L_, _TEX_l_, //Lambda 11.
-    _TEX_M_, _TEX_m_, //Mu 12.
-    _TEX_N_, _TEX_n_, //Nu 13.
-    _TEX_O_, _TEX_o_, //Omicron 15.
-    _TEX_P_, _TEX_p_, //Pi 16.
-    _TEX_Q_, _TEX_q_, //???
-    _TEX_R_, _TEX_r_, //Rho 17.
-    _TEX_S_, _TEX_s_, //Sigma 18.
-    _TEX_T_, _TEX_t_, //Tau 19.
-    _TEX_U_, _TEX_u_, //Theta 8.
-    _TEX_V_, _TEX_v_, //Omega 24.
-    _TEX_W_, _TEX_w_, //
-    _TEX_X_, _TEX_x_, //Chi 22.
-    _TEX_Y_, _TEX_y_, //Upsilon 20.
-    _TEX_Z_, _TEX_z_, //Zeta 6.
-  //LATEX FUNCTIONS
-  //Environments
-    _BMAT,
-    _MAT,
-    _PMAT,
-    _VMAT,
-    _LCAS,
-    _RCAS,
-    //Spacing
-    _TEX_SPACE,
-    _TEX_QUAD,
-    //Arrows
-    _UP_AR,
-    _LE_AR,
-    _DN_AR,
-    _RG_AR
-};
-
-static char *tex_greek_chars[] ={
-  "Alpha","alpha","Beta","beta","Psi","psi","Delta","delta","Epsilon","varepsilon","Phi","phi","Gamma","gamma","Eta","eta",
-  "Iota","iota","Xi","xi","Kappa","kappa","Lambda","lambda","Mu","mu","Nu","nu","Omicron","omicron","Pi","pi","x","x","Rho",
-  "rho","Sigma","sigma","Tau","tau","Theta","theta","Omega","omega","x","x","Chi","chi","Upsilon","upsilon","Zeta","zeta"
-};
-static char *tex_functions[] ={
-  //Spacing
-  "\\space",
-  "\\quad",
-  //Single Arrows
-  "uarr", 
-  "larr",
-  "darr",
-  "rarr"
-};
-static char *tex_environments[] ={
-  //Environments
-  "bmatrix", // Matrix []
-  "matrix",  // Matrix without brackets
-  "pmatrix", // Matrix ()
-  "vmatrix", // Matrix ||
-  "cases",   // Cases  {
-  "rcases"   // Cases  }
-};
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -174,24 +87,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 
-    // The FN key by default maps to a momentary toggle to layer 1 to provide access to the RESET key (to put the board into bootloader mode). Without
-    // this mapping, you have to open the case to hit the button on the bottom of the PCB (near the USB cable attachment) while plugging in the USB
-    // cable to get the board into bootloader mode - definitely not fun when you're working on your QMK builds. Remove this and put it back to KC_RGUI
-    // if that's your preference.
-    //
-    // To put the keyboard in bootloader mode, use FN+backspace. If you accidentally put it into bootloader, you can just unplug the USB cable and
-    // it'll be back to normal when you plug it back in.
-    //
-    // This keyboard defaults to 6KRO instead of NKRO for compatibility reasons (some KVMs and BIOSes are incompatible with NKRO).
-    // Since this is, among other things, a "gaming" keyboard, a key combination to enable NKRO on the fly is provided for convenience.
-    // Press Fn+N to toggle between 6KRO and NKRO. This setting is persisted to the EEPROM and thus persists between restarts.
-
     [_BASE] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          TO(_TEX_ACTIVE),
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,                   TO(_BASE_TEXT),
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_F13 , KC_ENT,           TO(_BASE_CODE),
-        KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
+        KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_DEL,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_APP,  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     //Latex
@@ -200,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          TO(_BASE),
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   TO(_BASE_TEXT),
-      TO(_TEX), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          TO(_BASE_CODE),
+        TO(_TEX),_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          TO(_BASE_CODE),
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, MO(_KYBS),
         _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______
     ),
@@ -225,15 +126,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          TO(_TEX_ACTIVE),
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,                   TO(_BASE),
-  TO(_TEXT_DE), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_F13 , KC_ENT,           TO(_BASE_CODE),
+    TO(_TEXT_DE),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_F13 , KC_ENT,           TO(_BASE_CODE),
         KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   MO(_KYBS),
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_APP,  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_TEXT_DE] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          X(SNEK),
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          TO(_BASE),
-        _______, _______, _______, _______, _______,_______,_______,XP(_ue,_UE),_______,XP(_oe,_OE),_______,_______,_______,                   TO(_BASE_TEXT),
-  TO(_BASE_TEXT),XP(_ae,_AE),X(_sz),_______,_______,_______, _______, _______, _______, _______, _______, _______, _______, _______,          TO(_BASE_CODE),
+        _______, _______, _______, X(_EU),  _______,_______,_______,XP(_ue,_UE),_______,XP(_oe,_OE),_______,_______,_______,                   TO(_BASE_TEXT),
+  TO(_BASE_TEXT),XP(_ae,_AE),X(_sz),_______,_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          TO(_BASE_CODE),
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, MO(_KYBS),
         _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______
     ),
@@ -250,7 +151,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_KYBS] = LAYOUT(
         _______, KC_MYCM, KC_WHOM, KC_CALC, KC_MSEL, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, _______,   RESET,          _______,
-        _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   RESET,          TO(_BASE),
+        _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   RESET,          _______,
         _______, _______, RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
         _______, _______, RGB_VAD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, RGB_HUI, _______, _______, _______, NK_TOGG, _______, _______, _______, _______,          _______, RGB_MOD, _______,
